5bf6e37e5a5c4c564b8c482b43690d595270b856
hlst <hoooooosety@gmail.com>
2021-10-11 21:42:28 +0800

    Initial Commit

---
+++ katatrain/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  katatrain
+//
+//  Created by 黄轶明 on 2021/10/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ katatrain/katatrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  katatrainApp.swift
+//  katatrain
+//
+//  Created by 黄轶明 on 2021/10/11.
+//
+
+import SwiftUI
+
+@main
+struct katatrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
